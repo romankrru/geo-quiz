8a8798f7d244966cc3f4e35088d37ce9d13dcbe9
Roman Krasikov <romankrru@yandex-team.ru>
2026-04-28 11:49:44 +0300

    Добавить vanillaExtract

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,7 +1,8 @@
 import { type ComponentProps } from 'react'
+import * as style from './Buttons.css'
 
 type ButtonProps = ComponentProps<'button'>
 
-export const Button = (props: ButtonProps) => {
-  return <button {...props} />
+export const Button = ({ className, ...props }: ButtonProps) => {
+  return <button {...props} className={style.button} />
 }
