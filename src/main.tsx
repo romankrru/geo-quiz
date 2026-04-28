e96ae669812f576b3764374b338260c7bf9fe476
Roman Krasikov <romankrru@yandex-team.ru>
2026-04-28 09:59:37 +0300

    Remove unused files; add normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { router } from './router'
 import './index.css'
+import 'normalize.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
